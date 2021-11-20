989137bd841c8c0e9ede3bd12eb01881efb95923
Orincy Whyte <orincywhyte@gmail.com>
2021-11-20 14:30:00 -0500

    init

---
+++ script.js
@@ -0,0 +1,19 @@
+
+
+/* const userName = prompt('What is your name?');
+
+if (userName === 'Shawn'){
+  document.write(`Hi ${userName}`);
+} ;
+
+ let today = new Date();
+let hourNow = today.getHours();
+
+const greeting = () => {
+if (hourNow > 18) {
+    greeting = 'Good evening, !' + userName;
+} else if (hourNow > 12) {
+    greeting = 'Good Afternoon, !' + userName;
+} else if (hourNow > 0) {
+    greeting = 'Good Morning, !' + userName;
+} ; */
