464ff9d89462989d33649d625085598ca0f0e29e
Orincy Whyte <orincywhyte@gmail.com>
2021-11-21 13:18:19 -0500

    commit

--- script.js
+++ script.js
@@ -1,19 +1,10 @@
 
 
-/* const userName = prompt('What is your name?');
+ const userName = prompt('What is your name?');
 
 if (userName === 'Shawn'){
-  document.write(`Hi ${userName}`);
+  document.write(`Hi ${userName}`) 
+  else (userName !'Shawn') {
+    document.write('Hi, Stranger')
+  }
 } ;
-
- let today = new Date();
-let hourNow = today.getHours();
-
-const greeting = () => {
-if (hourNow > 18) {
-    greeting = 'Good evening, !' + userName;
-} else if (hourNow > 12) {
-    greeting = 'Good Afternoon, !' + userName;
-} else if (hourNow > 0) {
-    greeting = 'Good Morning, !' + userName;
-} ; */
