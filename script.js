c3c6c7bc6f02e2285015f13f2aac277fc4de25b1
Orincy Whyte <orincywhyte@gmail.com>
2021-11-21 13:30:08 -0500

    commit

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
+const userName = prompt('What is your name?'); {
 
-
- const userName = prompt('What is your name?');
-
-if (userName === 'Shawn'){
-  document.write(`Hi ${userName}`) 
-  else (userName !'Shawn') {
-    document.write('Hi, Stranger')
-  }
-} ;
+  if (userName === 'Shawn') {
+    document.write(`Hi ${userName}`)
+  } else {
+      document.write('Hi Stranger')
+    }
+};
